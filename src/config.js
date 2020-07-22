eb37795f60f99ca8a58162c69e5f44cc68b9d975
Ernest Chang <iattempt.net@gmail.com>
2020-07-22 23:57:54 +0800

    refactor: remove unused code

--- src/config.js
+++ src/config.js
@@ -1,20 +1,4 @@
 export const defaultConfig = Object.freeze({
-  keyColors: {
-    null: "rebeccapurple",
-    boolean: "rebeccapurple",
-    number: "rebeccapurple",
-    string: "rebeccapurple",
-    array: "rebeccapurple",
-    object: "rebeccapurple",
-  },
-  valueColors: {
-    null: "blue",
-    boolean: "blue",
-    number: "blue",
-    string: "indianred",
-    array: "black",
-    object: "black",
-  },
   ignorePathCallback: (path) => false,
   defaultExpandCallback: (path) => path.length <= 1,
 });
