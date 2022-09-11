c7ce5fcd2f6af4cc8eb9c82109f5b4fdf08626f3
Ernest <iendeavor.github.io@gmail.com>
2022-09-11 12:18:02 +0800

    docs: update example

--- example/index.js
+++ example/index.js
@@ -4,6 +4,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
     const app = Vue.createApp(ObjectVisualizer.ObjectVisualizer, {
       data,
       rootName: 'users',
+      getKeys: (object) => Object.keys(object).sort(),
     })
     app.mount(document.getElementById('app'))
   })
